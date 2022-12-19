0deb4444ee0e1a93cc80fd613af9e4007e2ec96a
supertren1 <supertren@gmail.com>
2022-12-19 13:59:50 +0100

    change files

--- agency-management-vpc.tf
+++ agency-management-vpc.tf
@@ -5,8 +5,8 @@ module "vpc" {
   cidr = "${var.cidr}"
 
   azs             = ["${var.azs1}", "${var.azs2}", "${var.azs3}"]
-  private_subnets = ["${var.Private-1a}", "${var.Private-1b}", "${var.Private-1c}"]
-  public_subnets  = ["${var.Public-1a}", "${var.Public-1b}", "${var.Public-1c}"]
+  private_subnets = ["${var.Private_subnet_1A}", "${var.Private_subnet_1B}", "${var.Private_subnet_2A}",  "${var.Private_subnet_2B}",  "${var.Private_subnet_3A}",  "${var.Private_subnet_3B}"]
+  public_subnets  = ["${var.Public_subnet_1}", "${var.Public_subnet_2}", "${var.Public_subnet_3}"]
 # intra_subnets =   ["10.0.51.0/24", "10.0.52.0/24", "10.0.53.0/24"]
   
   enable_nat_gateway = true
