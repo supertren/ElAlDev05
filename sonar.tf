6722db59898ef11f837e2d556080b3054a347160
supertren1 <supertren@gmail.com>
2022-12-29 00:56:06 +0100

    add vpc_id

--- sonar.tf
+++ sonar.tf
@@ -1,5 +1,6 @@
 resource "aws_instance" "sonar" {
     ami = "ami-0574da719dca65348"
     instance_type = "t2.large"
+    vpc_id = "${var.project_name}-${var.environment}-${var.vpc_name}"
     tags = { Name = "${var.environment}" }  
 }
