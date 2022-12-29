dffb7adc96bba53e0882ccfbd3ac7ed268cff1b4
supertren1 <supertren@gmail.com>
2022-12-29 15:39:07 +0100

    cf

--- getVpcs.tf
+++ getVpcs.tf
@@ -1,10 +1,14 @@
-locals {
-	vpc_name = "agency-management-vpc"
+data "aws_vpc" "default" {
+  default = true
 }
 
-data "aws_subnets" "vpc_subnets" {
+data "aws_subnets" "all" {
   filter {
-    name   = "agency-management-vpc"
-    values = [var.vpc_id]
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
   }
+}
+
+output "first_subnet_id" {
+	value = sort(data.aws_subnets.all.ids)[0]
 }
