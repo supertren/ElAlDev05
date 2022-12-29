d1dab4c4e1321555375a860976af4de3d70edf17
supertren1 <supertren@gmail.com>
2022-12-29 14:00:16 +0100

    cf

--- sonar.tf
+++ sonar.tf
@@ -1,14 +1,6 @@
-data "aws_subnet_ids" "private" {
-  vpc_id = var.vpc_id
-
-  tags = {
-    Tier = "Private"
-  }
-}
-
-resource "aws_instance" "app" {
-  for_each      = data.aws_subnet_ids.private.ids
-  ami = "ami-0574da719dca65348"
-  instance_type = "t2.micro"
-  subnet_id     = each.value
+resource "aws_instance" "sonar" {
+    ami = "ami-0574da719dca65348"
+    instance_type = "t2.micro"
+    subnet_id = "Private_subnet_1A"
+    tags = { Name = "${var.environment}" }  
 }
