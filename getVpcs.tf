922fe097910db4c8b7388fe77b92eeb11f93932f
supertren1 <supertren@gmail.com>
2022-12-29 15:44:31 +0100

    cf

--- getVpcs.tf
+++ getVpcs.tf
@@ -8,27 +8,3 @@ data "aws_vpc" "public" {
     values = [local.vpc_name]
   }
 }
-
-data "aws_subnets" "public" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.vpc.id]
-  }
-
-  filter {
-    name   = "tag:Name"
-    values = ["${local.vpc_name}-public-*"]
-  }
-}
-
-data "aws_subnets" "private" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.vpc.id]
-  }
-
-  filter {
-    name   = "tag:Name"
-    values = ["${local.vpc_name}-private-*"]
-  }
-}
