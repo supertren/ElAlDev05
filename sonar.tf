a00197924cb15b5d23fa8af6baa831088ad70b00
supertren <43068656+supertren@users.noreply.github.com>
2022-12-29 12:55:14 +0100

    Update sonar.tf

--- sonar.tf
+++ sonar.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "sonar" {
     ami = "ami-0574da719dca65348"
-    instance_type = "t2.large"
+    subnet_id = "${var.Private_subnet_1A}"
     vpc_id = "${var.project_name}-${var.environment}-${var.vpc_name}"
     tags = { Name = "${var.environment}" }  
 }
