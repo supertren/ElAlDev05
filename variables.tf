facd61ff122e613389175724e70a63c0c803d0ec
supertren <43068656+supertren@users.noreply.github.com>
2022-12-29 12:53:33 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,6 +1,16 @@
 variable "project_name" {}
 variable "environment" {}
 variable "vpc_name" {}
+variable "cidr" {} 
 variable "azs1" {}
 variable "azs2" {}
 variable "azs3" {}
+variable "Private_subnet_1A" {}
+variable "Private_subnet_1B" {}
+variable "Private_subnet_2A" {}
+variable "Private_subnet_2B" {}
+variable "Private_subnet_3A" {}
+variable "Private_subnet_3B" {}
+variable "Public_subnet_1" {}
+variable "Public_subnet_2" {}
+variable "Public_subnet_3" {}
