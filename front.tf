0f2430cc71d6742d8a0c594cdf8361a8a21dd8ca
supertren1 <supertren@gmail.com>
2022-12-29 17:08:31 +0100

    cf

--- front.tf
+++ front.tf
@@ -1,5 +1,6 @@
 resource "aws_instance" "front" {
     ami = "ami-0574da719dca65348"
+    subnet_id = element(data.aws_vpcs.foo.ids, 0)
     instance_type = "t2.micro"
     tags = { Name = "${var.environment}" }  
 }
