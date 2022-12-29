501e1f46b7d0e6944080d7445eb28d80aa27e6c5
supertren1 <supertren@gmail.com>
2022-12-29 16:48:52 +0100

    cf

--- getResources.tf
+++ getResources.tf
@@ -7,16 +7,3 @@ data "aws_vpcs" "foo" {
 output "foo" {
   value = data.aws_vpcs.foo.ids
 }
-
-data "aws_subnet_ids" "AM-dev-agency-management-vpc" {
-  vpc_id = ${value}
-}
-
-data "aws_subnet" "AM-dev-agency-management-vpc" {
-  for_each = data.aws_subnet_ids.AM-dev-agency-management-vpc.ids
-  id       = each.value
-}
-
-output "subnet_cidr_blocks" {
-  value = [for s in data.aws_subnet.AM-dev-agency-management-vpc : s.cidr_block]
-}
