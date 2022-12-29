893c9a080d5f9316959ec20ecaa656da6774e575
supertren1 <supertren@gmail.com>
2022-12-29 17:37:22 +0100

    cf

--- getResources.tf
+++ getResources.tf
@@ -8,4 +8,18 @@ output "vpc-id" {
   value = data.aws_vpcs.vpc-id.ids
 }
 
+variable "vpc" {}
+variable "ami" {}
 
+data "aws_vpc" "selected" {
+  tags {
+    Name = "${var.vpc}"
+  }
+}
+
+data "aws_subnet_ids" "private" {
+  vpc_id = "${data.aws_vpc.selected.id}"
+  tags {
+      Name	= "AM-dev-agency-management-vpc"
+      }
+}
