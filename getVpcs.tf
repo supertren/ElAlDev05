dbcd67ee2a8dc7e8f5e9d83e7f9bacbd9f3b1854
supertren1 <supertren@gmail.com>
2022-12-29 15:30:39 +0100

    cf

---
+++ getVpcs.tf
@@ -0,0 +1,34 @@
+locals {
+	vpc_name = "agency-management-vpc"
+}
+
+data "aws_vpc" "vpc" {
+  filter {
+    name   = "tag:Name"
+    values = [local.vpc_name]
+  }
+}
+
+data "aws_subnets" "public" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.vpc.id]
+  }
+
+  filter {
+    name   = "tag:Name"
+    values = ["${local.vpc_name}-public-*"]
+  }
+}
+
+data "aws_subnets" "private" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.vpc.id]
+  }
+
+  filter {
+    name   = "tag:Name"
+    values = ["${local.vpc_name}-private-*"]
+  }
+}
