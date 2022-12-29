d7918dbe0888fc0f1f6e6535b6d63a1dac7f143a
supertren1 <supertren@gmail.com>
2022-12-29 14:19:39 +0100

    cf

--- sonar.tf
+++ sonar.tf
@@ -1,6 +1,6 @@
 module "ec2_instance" {
   source = "terraform-aws-modules/ec2-instance/aws"
-  instance_name = "sonar"
+  name = "sonar"
   instance_type = "t2.micro"
   ami = "ami-0574da719dca65348"
   subnet_id = "${element(module.vpc.private_subnets, 0)}"
