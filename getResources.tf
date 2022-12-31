ca2f747f94a96cb3f5c5f54f8dc54174cd3d4b82
supertren1 <supertren@gmail.com>
2022-12-31 02:50:00 +0100

    a

--- getResources.tf
+++ getResources.tf
@@ -1,13 +1,13 @@
 data "aws_vpc" "selected" {
   filter {
     name = "tag:vpc_name"
-    values = ["vpc_name	agency-management-vpc"]
+    values = ["agency-management-vpc"]
   }
 }
 
 data "aws_subnet" "selected" {
   filter {
     name = "tag:vpc_name"
-    values = ["vpc_name	agency-management-vpc"]
+    values = ["agency-management-vpc"]
   }
 }
