9658166461541dfe3e7d99af15e71a26513d786d
supertren1 <supertren@gmail.com>
2022-12-31 03:04:09 +0100

    d

--- gitlab-runner.tf
+++ gitlab-runner.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "gitlab-runner" {
     ami = "ami-0574da719dca65348"
-    subnet_id = element(data.aws_subnet.selected.id, 0)
+    subnet_id = element(data.aws_subnets.selected.id, 0)
     instance_type = "t2.micro"
     tags = { Name = "${var.environment}" }  
 }
