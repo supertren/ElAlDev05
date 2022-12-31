6d732ec787046517e7d67e0cc4ad87740a48af1c
supertren1 <supertren@gmail.com>
2022-12-31 03:32:28 +0100

    s

---
+++ getResources2.tf
@@ -0,0 +1,17 @@
+data "aws_subnets" "private" {
+  filter {
+    name   = "vpc-id"
+    values = [var.vpc_id]
+  }
+
+  tags = {
+    Name  = "AM-dev-agency-management-vpc"
+     }
+}
+
+resource "aws_instance" "app" {
+  for_each      = toset(data.aws_subnets.private.ids)
+  ami           = var.ami
+  instance_type = "t2.micro"
+  subnet_id     = each.value
+}
