4db02b99ba3aa4be614b52354a522d8131e183cf
supertren1 <supertren@gmail.com>
2022-12-31 02:57:30 +0100

    s

--- sonar.tf
+++ sonar.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "sonar" {
     ami = "ami-0574da719dca65348"
-    subnet_id = element(data.aws_vpcs.vpc-id.ids, 0)
+    subnet_id = element(data.aws_subnet.selected.id, 0)
     instance_type = "t2.micro"
     tags = { Name = "${var.environment}" }  
 }
