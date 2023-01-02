60a724c94bfc97291e4113bb5f849b5d7ea851e5
supertren1 <supertren@gmail.com>
2023-01-02 13:20:16 +0100

    s

--- sonar.tf
+++ sonar.tf
@@ -1,6 +1,10 @@
-# resource "aws_instance" "sonar" {
-#     ami = "ami-0574da719dca65348"
-#     subnet_id = "${data.aws_subnets.selected.id}"
-#     instance_type = "t2.micro"
-#     tags = { Name = "${var.environment}" }  
-# }
+locals {
+  subnets = concat(data.aws_subnets.example.ids)
+}
+
+resource "aws_instance" "sonar" {
+    ami = "ami-0574da719dca65348"
+    subnet_id = local.subnets[count.index]
+    instance_type = "t2.micro"
+    tags = { Name = "${var.environment}" }  
+}
