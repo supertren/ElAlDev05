76c376b86b44199684e0e6cd18bed62a6a5383ed
supertren1 <supertren@gmail.com>
2023-01-02 12:51:47 +0100

    s

--- front.tf
+++ front.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "front" {
      ami = "ami-0574da719dca65348"
      instance_type = "t2.micro"
-     subnet_id = ${data.aws_subnets.example.ids}
+     subnet_id = toset(data.aws_subnets.example.ids)
      tags = { Name = "front_${var.environment}" }  
  }
