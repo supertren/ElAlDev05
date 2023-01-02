76cb1489a3f0e1778ee663ff5ebb96d15c8d56c1
supertren1 <supertren@gmail.com>
2023-01-02 12:33:46 +0100

    s

--- front.tf
+++ front.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "front" {
-     for_each = data.aws_subnets.example.ids
+     for_each = toset (data.aws_subnets.example.ids)
      ami = "ami-0574da719dca65348"
      instance_type = "t2.micro"
      subnet_id = each.value
