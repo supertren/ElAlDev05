298ad39775d36003bb878275a0e3b031c9f2fdb7
supertren1 <supertren@gmail.com>
2023-01-02 12:41:41 +0100

    d

--- front.tf
+++ front.tf
@@ -1,7 +1,6 @@
 resource "aws_instance" "front" {
-     for_each = ${data.aws_subnets.example.id}
      ami = "ami-0574da719dca65348"
      instance_type = "t2.micro"
-     subnet_id = each.value
+     subnet_id = ${data.aws_subnets.example.id}
      tags = { Name = "sonar_${var.environment}" }  
  }
