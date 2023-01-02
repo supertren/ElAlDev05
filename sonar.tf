bbe78006dc78bbfea90a9cfd0eb3fafbdf4633ef
supertren1 <supertren@gmail.com>
2023-01-02 13:27:00 +0100

    a

--- sonar.tf
+++ sonar.tf
@@ -1,10 +1,6 @@
-locals {
-  subnets = concat("${data.aws_subnets.example.ids}")
-}
-
 resource "aws_instance" "sonar" {
     ami = "ami-0574da719dca65348"
-    subnet_id = local.subnets[count.index]
+    subnet_id = flatten(data.aws_subnets.example.ids)[count.index]
     instance_type = "t2.micro"
     tags = { Name = "sonar_${var.environment}" }  
 }
