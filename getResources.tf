9daf6c67f01e050aba51951b8e70cd5703b458d2
supertren1 <supertren@gmail.com>
2023-01-02 11:43:05 +0100

    d

--- getResources.tf
+++ getResources.tf
@@ -22,8 +22,8 @@ output "my_id2" {
 
 data "aws_subnets" "example" {
   filter {
-    name   = "vpc-id"
-    values = [var.vpc_id]
+    name = "tag:vpc_name"
+    values = ["agency-management-vpc"]
   }
 }
 
