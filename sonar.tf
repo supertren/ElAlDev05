cdfd55118a67c8847bc0bdc48f71a5e06d601df5
supertren1 <supertren@gmail.com>
2023-01-02 13:35:03 +0100

    s

--- sonar.tf
+++ sonar.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "sonar" {
     ami = "ami-0574da719dca65348"
-    subnet_id = (data.aws_subnets.example.ids)[*]
+    subnet_id = (data.aws_subnets.example.[count.index]ids)
     instance_type = "t2.micro"
     tags = { Name = "sonar_${var.environment}" }  
 }
