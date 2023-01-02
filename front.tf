a6643101a0d1b2d6f7d3fed827509cc46729ad40
supertren1 <supertren@gmail.com>
2023-01-02 12:24:14 +0100

    a

--- front.tf
+++ front.tf
@@ -1,6 +1,6 @@
-# resource "aws_instance" "front" {
-#     ami = "ami-0574da719dca65348"
-#     subnet_id = "${data.aws_subnets.selected.id}"
-#     instance_type = "t2.micro"
-#     tags = { Name = "${var.environment}" }  
-# }
+resource "aws_instance" "front" {
+     ami = "ami-0574da719dca65348"
+     subnet_id = "${data.aws_subnets.example.ids}"
+     instance_type = "t2.micro"
+     tags = { Name = "${var.environment}" }  
+ }
