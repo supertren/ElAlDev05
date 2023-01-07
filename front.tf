857033565514f0f1114d726000c2d06c77f0901e
miguel <miguel.alvarez@mindcurv.com>
2023-01-08 00:19:59 +0100

    a

--- front.tf
+++ front.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "front" {
      ami = "ami-0574da719dca65348"
      instance_type = "t2.micro"
-     subnet_id = flatten(data.aws_subnets.example.ids)[0]
+     subnet_id = flatten(data.aws_subnets.example.ids)[-1]
      tags = { Name = "front_${var.environment}" }  
  }
