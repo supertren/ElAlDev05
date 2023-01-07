237cee2832bdae1d16ff57f52913149ded0c7cc2
miguel <miguel.alvarez@mindcurv.com>
2023-01-08 00:34:49 +0100

    a

--- gitlab-runner.tf
+++ gitlab-runner.tf
@@ -2,5 +2,5 @@ resource "aws_instance" "gitlab-runner" {
      ami = "ami-0574da719dca65348"
      instance_type = "t2.micro"
      subnet_id = flatten(data.aws_subnets.example.ids)[0]
-     tags = { Name = "front_${var.environment}_gitlab-runner" }  
+     tags = { Name = "${var.environment}_gitlab-runner" }  
  }
