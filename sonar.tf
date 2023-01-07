056ffeb298689fdb5787bfeb7145525f4f78ed07
miguel <miguel.alvarez@mindcurv.com>
2023-01-08 00:32:45 +0100

    a

--- sonar.tf
+++ sonar.tf
@@ -2,5 +2,5 @@ resource "aws_instance" "sonar" {
      ami = "ami-0574da719dca65348"
      instance_type = "t2.micro"
      subnet_id = flatten(data.aws_subnets.example.ids)[0]
-     tags = { Name = "front_${var.environment}" }  
+     tags = { Name = "front_${var.environment}_sonar" }  
  }
